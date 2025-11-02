9619bbfbe491e831262affae8ea74511e1e2dfb0
Muhammad Shahzeel <shazeelarain786@gmail.com>
2025-11-02 05:58:08 +0500

    added categories table

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BulkyWeb.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<Category> Categories { get; set; } 
+
+
+    }
+}
