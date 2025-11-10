a343e91af1dea828fb6dae4231ff1e3c8b9e91eb
Muhammad Shahzeel <shazeelarain786@gmail.com>
2025-11-11 03:54:57 +0500

    created repository/repository pattern

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e148244737fc0d89ad6786599bac3a2baf8233b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d3faeb1267896ee171dbc966087620843d55404")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
