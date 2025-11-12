c5dbfc92ed675c6789395ed18493493c66c7ea8a
Muhammad Shahzeel <shazeelarain786@gmail.com>
2025-11-12 05:35:32 +0500

    added product repository & unit of work

--- Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
+++ Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db2b0fccea298057c4c7635483213acbe3e6d8b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aec9bf4912ec3b997aee7fd3cd01ca9c1fc61f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
