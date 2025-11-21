f3ca4cf548adefbb4dbd01d9e7ba1645c6e92c97
Muhammad Shahzeel <shazeelarain786@gmail.com>
2025-11-22 02:41:10 +0500

    handle image on edit roght now image update if its null for some reason application crashes

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+340d051156b5d3344ba1c98dc71122e035e4ad2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f62c49df6eff007c43442f2d6bdfd55156d87623")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
