32d114925c3146cef5826ffbd5bbdaaa9a2dfa9f
Muhammad Shahzeel <shazeelarain786@gmail.com>
2025-12-28 02:30:44 +0500

    created product details page

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96ef44178eff1161bc41bee50a3c4c185eb2d89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c2af886c693360629997ba113f395758cf81813")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
